f7fc7d8d10f64b1e9c8e64815c487766021eaa87
Chris "Slothy <chrisbennett555@yahoo.com>
2023-12-28 22:33:11 -0600

    1.0 :white_check_mark:

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+--FiveM Clear Area
+-- Version 1.0
+
+version '1.0.0'
+author 'Im2Slothy#0'
+description 'Clear Area around you'
+repository 'https://github.com/Im2Slothy/FiveM-AI-ChatGPT-Conversation'
+
+-- compatibility wrapper
+fx_version 'adamant'
+game 'common'
+
+-- Add a client script 
+client_script "client.lua"
